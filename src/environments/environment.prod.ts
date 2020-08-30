3a0d94da667994b523542f849049ce4fd267560b
blee1710 <s3707616@student.rmit.edu.au>
2020-08-30 20:22:12 +1000

    Added firebase to project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAC9MuiwCkjZJ8dxWhM6u27M_VsXV6Xm3c",
+    authDomain: "kanban-angular-3cb76.firebaseapp.com",
+    databaseURL: "https://kanban-angular-3cb76.firebaseio.com",
+    projectId: "kanban-angular-3cb76",
+    storageBucket: "kanban-angular-3cb76.appspot.com",
+    messagingSenderId: "8996320363",
+    appId: "1:8996320363:web:e9d0e2ce0a120b488aed2e",
+    measurementId: "G-Q0ZKE9MJF5"
+  }
 };
